26a57ac0e0251c47429de4ca56f278d944e1f236
Reese Dame <reesedame@gmail.com>
2023-05-31 09:53:07 -0700

    Complete Part 1

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+finches = [
+    {"species": "American Goldfinch", "scientificName": "Spinus tristis"},
+    {"species": "Northern Cardinal", "scientificName": "Cardinalis cardinalis"},
+]
+
+
+def home(request):
+    return render(request, "home.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def finches_index(request):
+    return render(request, "finches/index.html", {"finches": finches})
