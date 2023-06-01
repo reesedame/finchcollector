9659d7152a686bef3876395979f550c7c91ac393
Reese Dame <reesedame@gmail.com>
2023-06-01 11:27:04 -0700

    Add create functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Finch
+from django.views.generic.edit import CreateView
 
 
 def home(request):
@@ -18,3 +19,8 @@ def finches_index(request):
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
     return render(request, "finches/detail.html", {"finch": finch})
+
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = ["species", "scientific_name", "conservation_status"]
