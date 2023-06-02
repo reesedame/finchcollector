bcac0387dad3052f0c4ce01d994349269c02ad5e
Reese Dame <reesedame@gmail.com>
2023-06-02 15:44:28 -0700

    Add new feeding functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path(
         "finches/<int:pk>/delete/", views.FinchDelete.as_view(), name="finches_delete"
     ),
+    path("finches/<int:finch_id>/add_feeding/", views.add_feeding, name="add_feeding"),
 ]
