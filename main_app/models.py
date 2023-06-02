ad4985c10cdb5070559943305817a1ae269610c7
Reese Dame <reesedame@gmail.com>
2023-06-02 15:20:16 -0700

    Add Feeding model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 
+FOOD_TYPES = (("W", "Worm"), ("B", "Berry"), ("S", "Seed"))
+
 
 class Finch(models.Model):
     species = models.CharField(max_length=100)
@@ -12,3 +14,14 @@ class Finch(models.Model):
 
     def get_absolute_url(self):
         return reverse("detail", kwargs={"finch_id": self.id})
+
+
+class Feeding(models.Model):
+    date = models.DateField()
+    food_type = models.CharField(
+        max_length=1, choices=FOOD_TYPES, default=FOOD_TYPES[0][0]
+    )
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Fed {self.get_food_type_display()} on {self.date}"
